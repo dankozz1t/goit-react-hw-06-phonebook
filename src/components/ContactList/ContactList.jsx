2b02b76afab8db022262ce9b80534eb20dab5d85
Alex-Taylor Danko <alexdankoxxl@gmail.com>
2022-09-22 11:24:50 +0300

    add selector getFilteredContacts

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,42 +1,27 @@
-import React, { useEffect, useMemo } from 'react';
+import React from 'react';
 
 import { useSelector, shallowEqual } from 'react-redux';
 
-import { getFilter, getContacts } from '../../redux/selectors';
-
-import { ConfettiContainer } from '../Confetti/Confetti';
-import { login } from '../Confetti/utils';
+import { getFilteredContacts } from '../../redux/selectors';
 
 import { ContactItem } from '../ContactItem/ContactItem';
 
 import s from './ContactList.module.css';
 
 export function ContactList() {
-  const contacts = useSelector(getContacts, shallowEqual);
-  const filter = useSelector(getFilter, shallowEqual);
+  const filteredContacts = useSelector(getFilteredContacts, shallowEqual);
 
-  useEffect(() => {
-    login.submit();
-  }, [contacts]);
-
-  const renderContacts = useMemo(() => {
-    return contacts.filter(({ name }) =>
-      name.toLowerCase().includes(filter.toLowerCase().trim())
-    );
-  }, [filter, contacts]);
-
-  if (renderContacts.length === 0) {
+  if (filteredContacts.length === 0) {
     return <></>;
   }
 
-  const elements = renderContacts.map(({ id, name, number }) => (
+  const elements = filteredContacts.map(({ id, name, number }) => (
     <ContactItem key={id} id={id} name={name} number={number} />
   ));
 
   return (
     <>
       <ul className={s.list}>{elements}</ul>
-      <ConfettiContainer />
     </>
   );
 }
